db5314d913cfd98c558d81088b80884c7d1b175b
Paulo Reis <prsc.reis@gmail.com>
2023-04-01 19:52:25 -0300

    Challenge items implemented

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers as { user_id: string };
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.send(users);
+    } catch (error) {
+      return response.status(400).send({ error: error.message });
+    }
   }
 }
 
